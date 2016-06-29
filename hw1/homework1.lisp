1e15e26f27e7d43cd2a3fadcf67cd2e236eaa7b6
joshua <joshualilly@me.com>
2016-06-28 22:52:15 -0700

    Adding homework 1

---
+++ hw1/homework1.lisp
@@ -0,0 +1,32 @@
+
+;;1. (4p) Give iterative and recursive definitions of a function that
+
+;;   (a) takes a positive integer and prints that many dots.
+(defun iterativeDotGenerator (x) 
+  (loop
+    for m from 1 to x
+      do (princ ".")
+  ) 
+)
+
+(defun recursiveDotGenerator (x)
+    (if (= x 1)
+      1
+      (
+        recursiveDotGenerator (- x 1) 
+      )
+    )
+    (princ ".")
+)
+;; (b) takes a list and returns the number of times the symbol a
+;;       occurs in it.;;    
+(defun iterativeLetterCount (list) 
+  (let ((count 0)) 
+    (loop for sym in list do 
+      (if (eq sym 'a) 
+        (setq count (+ count 1) ) 
+      ) 
+    ) 
+    (princ count)
+  ) 
+)
