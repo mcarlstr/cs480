e839a4c1c3fae4137ad448ebd6696965b434e82f
joshua <joshualilly@me.com>
2016-06-29 21:38:23 -0700

    Finished problem 2

--- hw1/homework1.lisp
+++ hw1/homework1.lisp
@@ -11,6 +11,7 @@
 
 (defun recursiveDotGenerator (x)
     (if (= x 1)
+      ;; base case just return 1
       1
       (
         recursiveDotGenerator (- x 1) 
@@ -31,17 +32,50 @@
   ) 
 )
 
-
-
-
-;;(let ((counter 0)) 
-;;(defun countLetterperm (lis) 
-;;  (if (null lis) (setf counter nil) 
-;;    (if (eq (first lis) 'a) (progn (countLetter (rest lis)) (setq counter (+ counter 1))) (countLetter (rest lis))) )))
-
 (defun recursiveCountLetter (lis) 
   (let ((counter 0)) 
   (if (null lis) 0 
     (if (eq (first lis) 'a) 
       (setq counter (+ 1 (countLetter (rest lis)))) 
       (countLetter (rest lis))) )))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; 2. (3p) A friend is trying to write a function that returns the sum of all
+;;   the non-nil elements in a list. He has written two versions of this 
+;;   function and neither of them work. Explain what is wrong with each,
+;;   and give a correct version:
+
+;;(A) (defun summit (lst)
+;;      (remove nil lst)
+;;        (apply #'+ lst))
+
+;; ***** This function fails due to the remove function not modifying
+;;       the actual lst variable, so when the nil is removed it is not 
+;;       removed from the same list that gets the + operator applied to it
+;;       The working version is:
+
+(defun summit (lst) 
+  (setq lst (remove nil lst))
+    (apply #'+ lst))
+
+;;   (b) (defun summit (lst)
+;;          (let ((x (car lst)))
+;;	     (if (null x)
+;;		 (summit (car lst))
+;;		 (+ x (summit (cdr lst))))))
+
+;; ***** This function causes an infinite loop because
+;;       the recursive function does not have a base case
+;;       i.e. the if clause does not contain a call that 
+;;       doesn't call the summit function again. 
+;; A corrected version can be seen below:
+
+(defun summit (lst) 
+  (let ((x (car lst))) 
+  (progn (if (null lst) 
+    0 
+    (if (null x) 
+      (summit (cdr lst)) 
+      (+ x (summit (cdr lst))))))))
+
