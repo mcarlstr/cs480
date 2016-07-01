fbeaa15193c7df53a05d05550323763b6b3aacbd
joshua <joshualilly@me.com>
2016-07-01 09:20:46 -0700

    Added problem 8 solution

--- hw1/homework1.lisp
+++ hw1/homework1.lisp
@@ -105,11 +105,13 @@
 )
 
 ;; (c) mapcar
-(defun pos+ (lst) 
+(defun pos_mapcar+ (lst) 
   (let ((applist () )) 
     (loop for x from 0 to (length lst) do 
       (setq applist (append applist (list x)) ))  
         (mapcar #'+ lst applist)  ))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;;4. (3p) Define a function f that takes one numeric argument, and returns the
 ;;   greatest argument passed to it so far:
@@ -127,3 +129,60 @@
 )
 )
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+;;5. Dictionary (3p):
+;;    You are given a dictionary in the form  of "exploded" symbols (e.g. 
+;;    (d e f u n), (s e t q), (s e t f)). Write a recursive function,
+;;    lookup, that takes a prefix of an exploded symbol (e.g., (s e t))
+;;    and a dictionary and returns the list of all items in the
+;;    dictionary that match the prefix. 
+;;(defun lookup (pre dict) () )
+;;(defun lookup (pre dict) () )
+;;  (defun extractPrefix (pre element) (loop for x in element for 0 to (length pre) do (princ x )) )
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;6. Occurrences (3p):
+;;    Define a function that takes a list and returns a list indicating
+;;    the number of times each (eql) element appears, sorted from most
+;;    common element to least common:
+    
+;;    > (occurrences '(a b a d a c d c a))
+;;    ((A 4) (C 2) (D 2) (B 1))
+(defun occurrences1 (lst) (let ((count 0) (appList () ))
+    (loop for x in lst do (if (eq (first lst) x ) (setq count (+ count 1) ) ) ) 
+(setq appList (append appList (list (first lst) count)) ) (setq lst (remove (first lst) lst) ) 
+(setq count 0) (princ applist) (princ count) ) )
+
+;;8. Conv16: (3p)
+;;    Write a function conv16 that takes an exploded hexadecimal
+;;    number-i.e. a list in which hexadecimal digits (0-9,a-f) are separated by
+;;    spaces-and returns its corresponding value. For example,
+
+;;    > (conv16 '(7 d d)) 
+;;    2013
+
+;;    Note that 7dd in a hexadecimal form can be converted into a decimal form
+;;    by the following rule (7*16 + 13)*16 + 13, i.e. multiply the first digit 
+;;    by 16, add the second digit,  multiply the result by 16, add the third 
+;;    digit, and so on. Note that you do not need a calculator to do this problem.
+
+;;(defun conv16 (lst &optional (value 0)) (if (null lst) value 
+;;    (progn (setq value (+ value (getNumber (first lst) ) ) ) (setq value (* value 16)) (conv16 (rest lst) value) ) ) )
+
+;; Start by mapping the hex non-numerics to their number representation
+(defun getNumber(l) (let ( (numberVal l) (temp (mapcar #'(lambda (x y) (list x y)) '(A B C D E F) '(10 11 12 13 14 15) ) )) 
+  (loop for x in temp do 
+    (if (eq (first x) l ) 
+      (setq numberVal (nth 1 x)) 
+    ) 
+  )
+  numberVal ) 
+)
+
+;; right one?
+(defun conv16 (lst &optional (value 0)) (if (eq 1 (length lst)) (+ value (getNumber (nth 0 lst))) 
+     (progn (setq value (+ value (getNumber (first lst) ) ) ) (setq value (* value 16)) (conv16 (rest lst) value) ) ) )
