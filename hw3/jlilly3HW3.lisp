465a57c55d6d5084e54ee8dc6163aed7a7f9c7fe
joshua <joshualilly@me.com>
2016-07-13 20:32:20 -0700

    backing up

--- hw3/jlilly3HW3.lisp
+++ hw3/jlilly3HW3.lisp
@@ -5,7 +5,7 @@
 ;; (defun pair (element lst) (let ((result () )) (setq result (mapcar #'(lambda(x) (list element x)) lst))))
 
 (defun generate-permutations (colorList len)
-  (let ((retList () ) (tempList () ) (temp ()) (counter 0) (permutations 0))
+  (let ((retList () ) (tempList () ) (temp ()) (permutations 0))
   (setq colorList (mapcar #'(lambda (x) (list x)) colorList))
   (setq retList colorList)
   (setq permutations (expt (length colorList) len))
@@ -20,7 +20,27 @@
     (setq retList tempList)
     (setq tempList () )
   )
-  (format t "Generated ~D permutations~%" counter)
   retList
   )
+)
+
+(defun remove-illigal-permutations (permutations assocList)
+  (let ((countryMap ()) (associations ()) (temp ()))
+    (setq countryMap (mapcar #'(lambda(x)(first x) ) assocList)) ;; The index is it's position in the permutation
+    (loop for x in assocList do
+      (setq temp (second x))
+      (loop for y in temp do
+        ;; associations will have the indicies to check 
+        (setq associations (append associations (list (position y countryMap :test #'equal))))
+      )
+      ;; check the permutations
+      ()
+    )
+  )
+)
+
+(defun find-colors (assocList colorList)
+  (let ((len (length assocList)) (permutations ()) )
+    (setq permutations (generate-permutations colorList len))
+  ) 
 )
