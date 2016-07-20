15b7feb81340ae6b6b2c8e33e666d37ea554073f
joshua <joshualilly@me.com>
2016-07-20 15:38:17 -0700

    algorithm produces same output as professors

--- project/project.lisp
+++ project/project.lisp
@@ -1,12 +1,12 @@
 
 ;; Maybe we can add the weights to the association list like this.
 ;; so I am going to write code assuming weights are represented like this.
-(defvar *australia* '( (WA ((NT 1) (SA 1))) 
-                       (NT ((WA 1) (SA 1) (Q 1) )) 
-                       (SA ((WA 1) (NT 1) (Q 1) (SW 1) (V 1))) 
-                       (Q ((NT 1) (SA 1) (SW 1))) 
-                       (SW ((SA 1) (Q 1) (V 1))) 
-                       (V ((SA 1) (SW 1))) 
+(defvar *australia* '( (WA (NT SA)) 
+                       (NT (WA SA Q )) 
+                       (SA (WA NT Q SW V)) 
+                       (Q (NT SA SW)) 
+                       (SW (SA Q V)) 
+                       (V (SA SW)) 
                        (TS  () ) ))
 
 (defun remove-zero-one (list)
@@ -19,8 +19,8 @@
   (loop while (and (not (null list)) (or (eq 0 (length (second (first list)))) 
     (eq 1 (length (second (first list)))))) do
     (remove-from-neighbors list (second (first list)) (first (first list)))
-    (setq list (remove (first list) list))
-    (setq list (sort list #'(lambda(x y)(< (length (second x)) (length (second y))))))
+    (setf list (delete (first list) list))
+    (setf list (sort list #'(lambda(x y)(< (length (second x)) (length (second y))))))
     (format t "list after deleting ")
     (princ list)
     (format t "~%")
@@ -40,19 +40,25 @@
       (loop for x in associationsList do
         ;; store the symbol in the association list
         ;; that we need to find and delete.
-        (setq sym1 (first x))
- 
+        (setq sym1 x)
+;;        (format t "********* x is ")
+;;        (princ x)
+;;        (format t "~%") 
+;;        (break)
         ;; find the list that contains a reference to
         ;; the element we are about to delete and remove 
         ;; the element we are deleting from that elements list.
         (loop for y in map do
-          (if (eq (first y) sym1)
-                 
+;;       (format t "********* y is ")
+;;        (princ y)
+;;        (format t "~%") 
+;;        (break)
+          (if (eq (first y) sym1)        
             ;; this is the list with the edge we want to remove
             ;; which means the second element of y is the list which
             ;; contains that element.
             (loop for z in (second y) do
-              (if (eq (first z) symbol)
+              (if (eq z symbol)
                 (progn
                   (format t "removing ")
                   (princ z)
@@ -104,28 +110,36 @@
   (let ((F () ) (Gi () ) (largest () ) (copy ()) )
     ;; make a deep copy for the functions to mutate
     ;; since we don't want to destry the origional just yet
-    (setf copy (copy countryList))
-    (setf (second copy) (copy-alist (second countryList)))
+ ;;   (setf copy (copy countryList))
+ ;;   (setf (second copy) (copy-alist (second countryList)))
 
-    (setq Gi (remove-zero-one copy))
+    (setf countryList (remove-zero-one countryList))
+    (princ countryList)
+    (break)
+    ;;(setq Gi (remove-zero-one countryList))
+     (setq Gi countryList)
     (loop while (not (null Gi)) do
       ;; At this point in the algorithm weight is going to
       ;; always be 1 so the smallest weight to degree ration
       ;; will be the node with the largest degree.
-      (setq largest (find-largest-degree copy))
-      
+     ;; (setq largest (find-largest-degree countryList))
+      (setq largest (last countryList))
+
       (format t "Removing ")
       (princ largest)
       (format t "~%")
        
-      (setq F (append F (list largest)))
-      (break)
+      (setq F (append F  largest))
+;      (break)
       (format t "The cutset is now ")
       (princ F)
       (format t "~%")
 
-      (setf copy (delete largest copy))
-      (remove-from-neighbors copy (second largest) (first largest))
+  ;;    (setf countryList (delete largest countryList :test #'equal))
+      (setf countryList (delete (first (last countryList)) countryList))
+      (princ countryList)
+      (break)
+      (remove-from-neighbors countryList (second (first largest)) (first (first largest)))
 
       ;; G1 at this point should be === to countryList \ all verticies
       ;; with 0 or one as their degree and without the largest
@@ -135,17 +149,22 @@
       ;; a new G1 which is equivalent to countryList without any nodes with
       ;; degree 0 or 1 (without the node we extracted in this iteration) and
       ;; that should be what we want... I think... that still needs to be tested
-      (setq Gi (remove-zero-one copy))
+      (setf countryList (remove-zero-one countryList))
+      ;;(setq Gi (remove-zero-one countryList))
+      (setq Gi countryList)
+      (format t "Gi is ")
+      (princ Gi)
+      (format t "~%")
     )
 
     ;;make tree
-    (loop for m in F do
-      (princ m)
-      (break)
-      (remove-from-neighbors countryList (second m) (first m))
-      (setf countryList (delete m countryList :test #'equal))
-    ) 
-    (princ countryList)   
+;;    (loop for m in F do
+;;      (princ m)
+;;      (break)
+;;      (remove-from-neighbors countryList (second m) (first m))
+;;      (setf countryList (delete m countryList :test #'equal))
+;;    ) 
+;;    (princ countryList)   
     ;; return the cut set the list passed in
     ;; is now the tree portion.
     F
